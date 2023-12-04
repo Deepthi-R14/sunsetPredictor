048c97f787615357c61b37cb7627358860f605aa
Deepthi-R14 <deepthiremala@gmail.com>
2023-12-04 12:13:25 +0530

    Base structure

--- predictor/src/App.js
+++ predictor/src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import React, {useState}  from "react";
 
 function App() {
+
+const url = `http://api.openweathermap.org/geo/1.0/direct?q=London&appid=5620f9a52de6aa33309ae43808a0eafc`
+const lon = url.lon
+const lat = url.lat 
+
   return (
     <div className="App">
-     
+     <div className="container">
+      <div className="top">
+        <div className="location">
+          <p>London</p>
+        </div>
+        <div className="comment"></div>
+      </div>
+      <div className="bottom">
+        <div className="temp">
+          <h3>40 deg</h3>
+        </div>
+        <div className="setime">
+          <p>6:00 pm</p>
+        </div>
+        <div className="humidity">
+          <p>68%</p>
+        </div>
+        <div className="wind">
+          <p>25 kmph</p>
+        </div>
+
+      </div>
+     </div>
     </div>
   );
 }
